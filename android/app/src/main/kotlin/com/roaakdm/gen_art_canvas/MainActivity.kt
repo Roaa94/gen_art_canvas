4ec44acdc5fbce1588ba2af7105c8210885565e2
Roaa94 <roaa94khaddam@gmail.com>
2023-09-16 23:19:17 +0200

    set up :construction:

---
+++ android/app/src/main/kotlin/com/roaakdm/gen_art_canvas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.roaakdm.gen_art_canvas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GenArtCanvas',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}

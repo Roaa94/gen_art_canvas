686d5089c708617cb4c479065ec1f39fc799ae1c
Roaa94 <roaa94khaddam@gmail.com>
2023-09-23 13:57:01 +0200

    fix: remove unused import

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';

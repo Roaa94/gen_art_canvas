de86ed6a574310cde820b607693ad57499479c49
Roaa94 <roaa94khaddam@gmail.com>
2023-09-25 01:00:08 +0200

    fix: artist page UI fixes

--- lib/cuboids/pages/cuboids_canvas_artist_page.dart
+++ lib/cuboids/pages/cuboids_canvas_artist_page.dart
@@ -3,8 +3,10 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gen_art_canvas/auth/application/auth_service.dart';
+import 'package:gen_art_canvas/auth/domain/artist.dart';
 import 'package:gen_art_canvas/auth/widgets/artist_home_info.dart';
 import 'package:gen_art_canvas/auth/widgets/artist_nickname_dialog.dart';
+import 'package:gen_art_canvas/core/style/app_colors.dart';
 import 'package:gen_art_canvas/cuboids/widgets/cuboids_creator_bottom_sheet.dart';
 import 'package:gen_art_canvas/settings/cuboids_canvas_settings.dart';
 import 'package:gen_art_canvas/settings/cuboids_canvas_settings_provider.dart';
@@ -24,40 +26,94 @@ class CuboidsCanvasArtistPage extends ConsumerWidget {
     final isAuthLoading = ref.watch(authArtistProvider).isLoading;
 
     return Scaffold(
-      body: authArtist == null
-          ? ArtistNicknameDialog(
-              onSubmit: (String nickname) => ref
-                  .read(authServiceProvider)
-                  .signArtistInAnonymously(nickname: nickname),
-              isLoading: isAuthLoading,
-            )
-          : ref.watch(cuboidsCanvasSettingsProvider).when(
-                data: (CuboidsCanvasSettings settings) => Column(
-                  children: [
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(top: 5, bottom: 10, left: 10),
-                      child: ArtistHomeInfo(
-                        authArtist,
-                        onSignOut: () =>
-                            ref.watch(authServiceProvider).signArtistOut(),
-                      ),
+      body: isAuthLoading
+          ? const Center(child: CircularProgressIndicator())
+          : authArtist == null
+              ? ArtistNicknameDialog(
+                  onSubmit: (String nickname) => ref
+                      .read(authServiceProvider)
+                      .signArtistInAnonymously(nickname: nickname),
+                  isLoading: isAuthLoading,
+                )
+              : ref.watch(cuboidsCanvasSettingsProvider).when(
+                    data: (CuboidsCanvasSettings settings) => Column(
+                      children: [
+                        _buildArtistInfo(
+                          context,
+                          artist: authArtist,
+                          onSignOut: () =>
+                              ref.watch(authServiceProvider).signArtistOut(),
+                        ),
+                        Expanded(
+                          child: CuboidsCreatorBottomSheet(
+                            settings: settings,
+                            authArtist: authArtist,
+                          ),
+                        ),
+                      ],
                     ),
-                    Expanded(
-                      child: CuboidsCreatorBottomSheet(
-                        settings: settings,
-                        authArtist: authArtist,
-                      ),
-                    ),
-                  ],
-                ),
-                error: (e, _) {
-                  log('Error fetching cuboids canvas settings');
-                  log(e.toString());
-                  return const Center(child: Text('An error occurred!'));
-                },
-                loading: () => const Center(child: CircularProgressIndicator()),
+                    error: (e, _) {
+                      log('Error fetching cuboids canvas settings');
+                      log(e.toString());
+                      return const Center(child: Text('An error occurred!'));
+                    },
+                    loading: () =>
+                        const Center(child: CircularProgressIndicator()),
+                  ),
+    );
+  }
+
+  Widget _buildArtistInfo(
+    BuildContext context, {
+    required Artist artist,
+    VoidCallback? onSignOut,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 5),
+      child: Row(
+        children: [
+          Expanded(
+            child: RichText(
+              text: TextSpan(
+                text: 'Welcome, ',
+                style: Theme.of(context).textTheme.bodySmall,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: '${artist.nickname}!',
+                    style: Theme.of(context)
+                        .textTheme
+                        .bodySmall!
+                        .copyWith(color: AppColors.primary),
+                  ),
+                ],
               ),
+            ),
+          ),
+          if (artist.createdCuboidsCount > 0)
+            Expanded(
+              child: Align(
+                alignment: Alignment.centerRight,
+                child: Text(
+                  'Crafted cuboids: ${artist.createdCuboidsCount}',
+                  style: Theme.of(context).textTheme.labelSmall!.copyWith(
+                        color: AppColors.googleBlue600,
+                      ),
+                ),
+              ),
+            ),
+          TextButton.icon(
+            onPressed: onSignOut,
+            icon: const Icon(
+              Icons.logout,
+              size: 12,
+            ),
+            label: const Text(
+              'Exit',
+              style: TextStyle(fontSize: 12),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
